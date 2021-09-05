cc8bb21dda8502bb04681ebff198ac867cfb6675
chloe me <albert@chloe.me>
2021-09-05 01:16:46 +0000

    save for now continue next time

--- src/views/maps_screen.js
+++ src/views/maps_screen.js
@@ -1,11 +1,25 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Dimensions} from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, Image} from "react-native";
 import MapView from 'react-native-maps';
+import { Icon } from 'react-native-elements'
+import { Card, Button,Title, Paragraph, Avatar } from 'react-native-paper';
+
 
 export default function MapScreen({navigation}) {
 
     return (
         <View>
+            <View style={styles.floatingCard}>
+                <Card>
+                    <Card.Content>
+                        <Title><Icon name='rowing'/>Searching ...</Title>
+                        <Paragraph>Card content</Paragraph>
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button>Cancel</Button>
+                    </Card.Actions>
+                </Card>
+            </View>
             <MapView style={styles.map} />
         </View>
     )
@@ -17,8 +31,16 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    floatingCard:{
+        position: 'absolute',
+        width: Dimensions.get('window').width * 0.9,
+        alignSelf:'center',
+        marginTop: 25,
+        zIndex: 10
+    },
     map: {
       width: Dimensions.get('window').width,
       height: Dimensions.get('window').height,
+      zIndex: -1
     },
   });
--- src/views/navigators/main_drawer.js
+++ src/views/navigators/main_drawer.js
@@ -3,6 +3,7 @@ import { DrawerContent } from './navigator_content/drawer_content';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import LoginScreen from '../login_screen';
+import HomeScreen from '../home_screen';
 import MyStackNavigator from './stack_navigator';
 
 const Drawer = createDrawerNavigator();
